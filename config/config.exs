c4592d66fc18dd3982e32d74ebe1a9788ec87ed4
Quoc Tan <darknight1604@gmail.com>
2021-10-18 20:59:37 +0700

    Update

--- config/config.exs
+++ config/config.exs
@@ -49,7 +49,7 @@ config :phoenix, :json_library, Jason
 config :hound,
     driver: "chrome_driver",
     browser: "chrome_headless",
-    host: "crawl-driver-1.herokuapp.com",
+    host: "https://crawl-driver-1.herokuapp.com",
     port: 9515
 
 # Import environment specific config. This must remain at the bottom
