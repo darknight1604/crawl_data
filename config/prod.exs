c1c55765abfe145fd3d0ce4d075e1e4cd3cafa95
Quoc Tan <darknight1604@gmail.com>
2021-10-18 11:18:30 +0700

    Update setting

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,9 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :hello, HelloWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  # url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "shopee-crawl-data.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
