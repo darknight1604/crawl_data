15c20d28cb550cfcd66dc18d75c95d5841665840
Quoc Tan <darknight1604@gmail.com>
2021-10-22 13:44:13 +0700

    Remove host

--- config/config.exs
+++ config/config.exs
@@ -48,9 +48,7 @@ config :phoenix, :json_library, Jason
 
 config :hound,
     driver: "chrome_driver",
-    browser: "chrome_headless",
-    host: "https://crawl-driver-1.herokuapp.com",
-    port: 9515
+    browser: "chrome_headless"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
